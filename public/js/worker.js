857c0784a83c1df2b50e302756b4727fad51ce5c
plant99 <shivashispadhi@gmail.com>
2018-04-20 13:54:01 +0530

    Add other origin files to cache

--- public/js/worker.js
+++ public/js/worker.js
@@ -7,7 +7,10 @@ const PRECACHE_URL_START = [
 	'/css/bootstrap.min.css',
 	'/js/main.js',
 	'/js/vendor/bootstrap.min.js',
-	'/js/vendor/jquery.min.js'
+	'/js/vendor/jquery.min.js',
+  'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js'
 ];
 
 
